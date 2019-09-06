c2c97a710d31d7e2a4773133f408ca468940bfe4
Nindy Haris Putri <nindyharis@student.telkomuniversity.ac.id>
2019-09-06 09:08:02 +0700

    add ToDo 2

--- app/src/main/java/d3ifcool/org/kolabapp/MainActivity.java
+++ app/src/main/java/d3ifcool/org/kolabapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //ToDo 1: Menambahkan 1 baris kode
-        //ToDo 2: Menambahkan 2 baris kode 
+        //ToDo 2: Menambahkan 2 baris kode
     }
 }
